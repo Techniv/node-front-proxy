fd3a7c4eeeb098cf2586d729b662f6fa96126db8
Vincent Peybernes [Techniv] <vpeybernes.pro@gmail.com>
2012-11-02 21:13:00 +0100

    Use cmd-conf to configure proxy.

--- proxy.js
+++ proxy.js
@@ -4,7 +4,9 @@ try{
 	require('colors');
 	var util		= require('util'),
 		http		= require('http'),
-		proxyLib	= require('http-proxy');
+		path		= require('path'),
+		proxyLib	= require('http-proxy'),
+		cmdConf		= require('cmd-conf');
 } catch(err){
 	console.log("ERROR: Can't load required modules. Launch `npm install`.");
 	console.log(err.stack);
@@ -16,11 +18,28 @@ console.log("Starting NODE-FRONT-PROXY".green);
 console.log("=========================");
 console.log("");
 
+// Get Application path.
+var appPath = path.dirname(process.mainModule.filename)+'/';
+
+// Loading config with cmd-conf.
+try {
+	var config = cmdConf.getParameters();
+} catch(err){
+	console.log("Loading proxy config:"+" FAIL".red);
+	console.log(err.stack.yellow);
+	process.exit(1);
+}
+console.log("Loading proxy config:"+" OK".green);
+console.log("Rule's file: "+config.ruleFile.yellow)
+console.log("Listen port: "+(""+config.listenPort).yellow)
+console.log("=========================");
+console.log("");
+// Loading rules.
 try{
-	var rules = require('./rules.js');
+	var rules = require(appPath+config.ruleFile);
 } catch (err){
 	console.log("Loading proxy rules:"+" FAIL".red);
-	console.log(err.stack.red);
+	console.log(err.stack.yellow);
 	process.exit(1);
 }
 console.log("Loading proxy rules:"+" OK".green);
@@ -30,5 +49,6 @@ console.log("Applicable rules:".yellow);
 for(var key in rules){
 	console.log(key.green+": "+util.inspect(rules[key]).yellow);
 }
-console.log("===================");
+console.log("=========================");
 console.log("");
+
